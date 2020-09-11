9f36b563d950a77e83c8cb42285b43bc14314ebe
MustafaCEVIK <cevikksoft@gmail.com>
2020-09-11 12:49:36 +0300

    Done EmailSenderTest unit tests.

--- Project2W1/src/test/java/org/kodluyoruz/trendyol/business/EmailSenderTest.java
+++ Project2W1/src/test/java/org/kodluyoruz/trendyol/business/EmailSenderTest.java
@@ -1,13 +1,59 @@
 package org.kodluyoruz.trendyol.business;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.kodluyoruz.trendyol.constant.ErrorMessage;
+import org.kodluyoruz.trendyol.exception.MessageLimitException;
+import org.kodluyoruz.trendyol.model.Company;
+import org.kodluyoruz.trendyol.model.Email;
+import org.kodluyoruz.trendyol.model.User;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 public class EmailSenderTest {
-    @Test
-    public void TestMethodX() {
 
-        assertThat("aa").isEqualTo("aa");
+    Company company;
+    Email email;
+    User user;
+
+    @BeforeEach
+    public void setUp() {
+        company = new Company("Comp1", 1); // language 1 -> TR
+        user = new User("User1");
+        email = new Email("Email Test", "Test");
+    }
+
+    @Test
+    public void it_should_send_four_email() {
+        // given
+        // defined fields -> BeforeEach method
+        int emailLimit = company.getEmailLimit();
+
+        // when
+        company.SendEmail(email, user);
+        company.SendEmail(email, user);
+        company.SendEmail(email, user);
+        company.SendEmail(email, user);
+
+        // then
+        assertThat(company.getEmailLimit()).isEqualTo(emailLimit - 4);
+    }
+
+    @Test
+    public void it_should_throw_exception_when_email_limit_exceeded() {
+        // given
+        // defined fields -> BeforeEach method
+        int emailLimit = company.getEmailLimit();
+        for (int i = 0; i < emailLimit; i++) {
+            company.SendEmail(email, user);
+        }
+
+        // when
+        Throwable throwable = catchThrowable(() -> company.SendEmail(email, user));
+
+        // then
+        assertThat(throwable).isInstanceOf(MessageLimitException.class);
+        assertThat(throwable).hasMessageContaining(ErrorMessage.messageLimitException(company.getLanguage()));
     }
 }
