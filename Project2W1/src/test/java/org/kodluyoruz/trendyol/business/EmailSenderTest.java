2f43d56540f441ddd4aa82f1f2fb95dbb8493007
MustafaCEVIK <cevikksoft@gmail.com>
2020-09-11 11:58:38 +0300

    Add junit & assertj package -> pom.xml.

---
+++ Project2W1/src/test/java/org/kodluyoruz/trendyol/business/EmailSenderTest.java
@@ -0,0 +1,13 @@
+package org.kodluyoruz.trendyol.business;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EmailSenderTest {
+    @Test
+    public void TestMethodX() {
+
+        assertThat("aa").isEqualTo("aa");
+    }
+}
